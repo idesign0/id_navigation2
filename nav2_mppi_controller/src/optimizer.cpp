9d9bad9ac48daa5f3fd34fd2c5c7c06bae65c7b9
Dhruv Patel <dhruvpatel2991998@gmail.com>
2026-03-11 17:10:35 +0100

    feat: optimize trajectory integration and fix Apple Clang template ambiguity (#6017)
    * feat: optimize trajectory integration and fix Apple Clang template ambiguity
    
    Signed-off-by: Dhruv Patel <dhruvpatel2991998@gmail.com>
    
    * added apple_utils and removed xt::masked_view
    
    Signed-off-by: Dhruv Patel <dhruvpatel2991998@gmail.com>
    
    * used inline cumsum instead of Typedefs and added Copyright
    
    Signed-off-by: Dhruv Patel <dhruvpatel2991998@gmail.com>
    
    * removed uncrusify changes and added manual loop in applyContraint
    
    Signed-off-by: Dhruv Patel <dhruvpatel2991998@gmail.com>
    
    * code style divergence fixes
    
    Signed-off-by: Dhruv Patel <dhruvpatel2991998@gmail.com>
    
    ---------
    
    Signed-off-by: Dhruv Patel <dhruvpatel2991998@gmail.com>

--- nav2_mppi_controller/src/optimizer.cpp
+++ nav2_mppi_controller/src/optimizer.cpp
@@ -344,7 +344,7 @@ void Optimizer::integrateStateVelocities(
   auto traj_y = xt::view(trajectory, xt::all(), 1);
   auto traj_yaws = xt::view(trajectory, xt::all(), 2);
 
-  xt::noalias(traj_yaws) = xt::cumsum(wz * settings_.model_dt, 0) + initial_yaw;
+  xt::noalias(traj_yaws) = cumsum_1d(wz * settings_.model_dt) + initial_yaw;
 
   auto yaw_cos = xt::roll(xt::eval(xt::cos(traj_yaws)), 1);
   auto yaw_sin = xt::roll(xt::eval(xt::sin(traj_yaws)), 1);
@@ -360,8 +360,8 @@ void Optimizer::integrateStateVelocities(
     dy = dy + vy * yaw_cos;
   }
 
-  xt::noalias(traj_x) = state_.pose.pose.position.x + xt::cumsum(dx * settings_.model_dt, 0);
-  xt::noalias(traj_y) = state_.pose.pose.position.y + xt::cumsum(dy * settings_.model_dt, 0);
+  xt::noalias(traj_x) = state_.pose.pose.position.x + cumsum_1d(dx * settings_.model_dt);
+  xt::noalias(traj_y) = state_.pose.pose.position.y + cumsum_1d(dy * settings_.model_dt);
 }
 
 void Optimizer::integrateStateVelocities(
@@ -371,7 +371,7 @@ void Optimizer::integrateStateVelocities(
   const float initial_yaw = static_cast<float>(tf2::getYaw(state.pose.pose.orientation));
 
   xt::noalias(trajectories.yaws) =
-    xt::cumsum(state.wz * settings_.model_dt, {1}) + initial_yaw;
+    cumsum_2d(state.wz * settings_.model_dt, 1) + initial_yaw;
 
   auto yaw_cos = xt::roll(xt::eval(xt::cos(trajectories.yaws)), 1, 1);
   auto yaw_sin = xt::roll(xt::eval(xt::sin(trajectories.yaws)), 1, 1);
@@ -387,9 +387,9 @@ void Optimizer::integrateStateVelocities(
   }
 
   xt::noalias(trajectories.x) = state.pose.pose.position.x +
-    xt::cumsum(dx * settings_.model_dt, {1});
+    cumsum_2d(dx * settings_.model_dt, 1);
   xt::noalias(trajectories.y) = state.pose.pose.position.y +
-    xt::cumsum(dy * settings_.model_dt, {1});
+    cumsum_2d(dy * settings_.model_dt, 1);
 }
 
 xt::xtensor<float, 2> Optimizer::getOptimizedTrajectory()
